a2a23627ed477cb6e23ae4957e3a8e55cf709d2e
raygamer <rayssapaz987@gmail.com>
2025-11-05 08:16:29 -0300

    Update tesouro.py
    atualizado

--- tesouro.py
+++ tesouro.py
@@ -1,10 +1,15 @@
 import pygame
 
 class Tesouro(pygame.sprite.Sprite):
-    def __init__(self, x, y, frase):
+    def __init__(self, x, y, frase, imagem_path="tesouro.png"):
         super().__init__()
-        self.image = pygame.Surface((30,30))
-        self.image.fill((255,215,0))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
+        try:
+            self.image = pygame.image.load(imagem_path).convert_alpha()
+            self.image = pygame.transform.scale(self.image, (30, 30))  # tamanho ajustável
+        except:
+            # Caso a imagem não carregue, cria um quadrado colorido
+            self.image = pygame.Surface((30, 30))
+            self.image.fill((255, 215, 0))  # amarelo dourado
+
+        self.rect = self.image.get_rect(topleft=(x, y))
         self.frase = frase
